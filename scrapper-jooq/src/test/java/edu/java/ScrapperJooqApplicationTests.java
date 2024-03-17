0f0389410a2df20f649a134ac97452680edee3fb
k0patych1 <367512@niuitmo.ru>
2024-03-17 23:56:14 +0300

    feat : add generated jooq

---
+++ scrapper-jooq/src/test/java/edu/java/ScrapperJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
