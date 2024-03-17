cb175244a2ea26576c34d905578561873b293b95
k0patych1 <367512@niuitmo.ru>
2024-03-17 21:01:01 +0300

    style: typo

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
-public class gtScrapperApplication {
+public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
     }
